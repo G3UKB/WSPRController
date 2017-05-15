5d9017fbabb6741b4ea351e69c800a4f40d594a0
G3UKB <bob@bobcowdery.plus.com>
2017-05-15 16:56:16 +0100

    Testing.

--- python/controller/wsprauto.py
+++ python/controller/wsprauto.py
@@ -731,12 +731,12 @@ class Automate:
         if subcommand == INVOKE:
             if self.__WSPRProc == None:
                 # Process not running, so start
-                self.__WSPRProc = subprocess.Popen(['python3', 'wspr'], cwd=WSPR_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                self.__WSPRProc = subprocess.Popen(['python3', 'wspr.py'], cwd=WSPR_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
                 sleep(1.0)
             else:
                 if self.__WSPRProc.poll() != None:
                     # Process was running but has terminated, so restart
-                    self.__WSPRProc = subprocess.Popen(['python3', 'wspr'], cwd=WSPR_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                    self.__WSPRProc = subprocess.Popen(['python3', 'wspr.py'], cwd=WSPR_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
                     sleep(1.0)
             return DISP_CONTINUE, None
         elif subcommand == RESET:
