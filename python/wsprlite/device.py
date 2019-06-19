57df4032fed1ffc3835b052d76e93bb986d85669
Bob Cowdery <bob@bobcowdery.plus.com>
2019-06-19 12:01:01 +0100

    Developing.

--- python/wsprlite/device.py
+++ python/wsprlite/device.py
@@ -21,10 +21,39 @@
 #     bob@bobcowdery.plus.com
 #
 
-# Current POC for RPi access to the WSPRLite Flexi transmitter
-
 """
+    Device driver for the WSPRLite WSPR transmitter.
+    
+    This is pretty much verbatim from the documentation to better understand the code.
+    
+    Physical connection
+    =====================
+
+    ## USB
+    
+    Communication between the config program and the WSPRlite is by means of a USB-to-UART serial bridge (currently a Silicon Labs CP2104 chip).
+    
+    The USB device description is set during manufacture to "SOTAbeams WSPRlite".
+    
+    Serial connection
+    =================
+    
+    Serial settings are:
+    
+    * Baud rate: 1Mbps
+    * 8 data bits, 2 stop bits, no parity
+    * Flow control: RTS/CTS
+    
+    Note: flow control is not fully supported.
+    
+    RTS (pause flow from WSPRlite to computer) is deliberately not implemented in the hardware, since the USB-to-UART chip has quite a large buffer (576 bytes), and the WSPRlite only ever sends data back to the computer in response to messages from the computer. To pause flow from the WSPRlite to computer, simply stop sending data from the computer to the WSPRlite.
+    
+    CTS (pause flow from computer to WSPRlite) does not appear to work correctly, at least on Linux. This might be a bug in the libserialport library. Workaround: only send one message at a time to the WSPRlite, wait for the response before sending the next message.
+    
+    Testpoints TP1 and TP2 on the WSPRlite board are connected to the RX and TX pins for the serial connection.
+
     Message format
+    ==============
     
     Integers are little-endian.
     
@@ -52,85 +81,228 @@
     
     The checksum is the CRC32 of `msgType msgData`.
     
-    The 'C' message type enumeration:
-    enum class MsgType
-    {
-    0    Version=0,
-    1    NACK,
-    2    ACK,
-    3    Read,
-    4    ResponseData,
-    5    Write,
-    6    Reset,
-    7    Bootloader_State,
-    8    Bootloader_Enter,
-    9    Bootloader_EraseAll,
-    a   Bootloader_ErasePage,
-    b   Bootloader_ProgramHexRec,
-    c   Bootloader_ProgramRow,
-    d   Bootloader_ProgramWord,
-    e   Bootloader_CRC,
-    f   Bootloader_ProgramResetAddr,
-    10   DeviceMode_Get,
-    11   DeviceMode_Set,
-    12   DumpEEPROM,
-    13   WSPR_GetTime,
-    14   TestCmd,
-    };
+    For message types, qualifiers and modes see enumerations.
     
-    // EEPROM variable IDs, for use with MsgType::Read and Write
-    enum class VarId
-    {
-    0    MemVersion=0,
-    1    xoFreq,
-    2    xoFreqFactory,
-    3    ChangeCounter,
-    4    DeviceId,
-    5    DeviceSecret,
-    6    WSPR_txFreq,
-    7    WSPR_locator,
-    8    WSPR_callsign,
-    9    WSPR_paBias,
-    a    WSPR_outputPower,
-    b    WSPR_reportPower,
-    c    WSPR_txPct,
-    d    WSPR_maxTxDuration,
-    e    CwId_Freq,
-    f    CwId_Callsign,
-    10    PaBiasSource,
-    11    END,
-    };
+    # Messages from WSPRlite to computer
 
-    enum class DeviceMode
-    {
-    0    Init=0,
-    1    WSPR_Pending=1,
-    2    WSPR_Active,
-    3    WSPR_Invalid,
-    4    Test_ConstantTx,
-    5    FactoryInvalid,
-    6    HardwareFail,
-    7    FirmwareError,
-    8    WSPR_MorseIdent,
-    9    Test,
-    };
+    The WSPRlite only sends messages in response to commands sent by the computer.
+    Note that there is no tracking built into the protocol of which command a message
+    is replying to, though the WSPRlite is guaranteed to process and respond to messages
+    sequentially. Unless you have a good reason to do otherwise, send one message at a
+    time and wait for a response before sending the next one, to avoid losing track of
+    which response was for which command.
+    
+    ### ACK
+    Indicates that the command was successful. No msgData.
+    
+    ### NACK
+    Indicates that the command was not successful.
+    msgData may be present. If it is, it will be a null-terminated string which is the error message.
+    
+    ### ResponseData
+    Indicates that the command was successful and has returned some data.
+    The meaning of msgData depends on what the original command was - see "from computer to WSPRlite" section below.
+    
+    # Messages from computer to WSPRlite
+    
+    ### Version
+    Retrieves information about firmware and hardware version. 
+    No command data.
+    Reply: ResponseData
+    
+        msgData ::= deviceVersion firmwareVersion
+        deviceVersion ::= productId productRevision bootloaderVersion
+        firmwareVersion ::=  major minor patch date
+    
+    All numbers (productId, productRevision, bootloaderVersion, major, minor, patch, date) are uint32.
+    
+    deviceVersion is currently 1,1,1 for the WSPRlite.
+    
+    ### Read
+    Read a config variable.
+    Command data:
+    
+        msgData ::= variableId
+        variableId ::= uint16
+    
+    Reply: NACK or ResponseData. Contents of ResponseData will depend on which variable is being read
+    - see cfgvars.md for details.
+    
+    ### Write
+    Write a config variable.
+    Command data:
+    
+        msgData ::= variableId variableData
+        variableId ::= uint16
+    
+    variableData will depend on which variable is being written - see cfgvars.md for details.
+    
+    Reply: ACK or NACK.
+    
+    ### Reset
+    Reboots the device. No command data. Reply: ACK.
+    
+    ### DeviceMode_Get
+    Gets some information about what the WSPRlite is currently doing. Supported by firmware v1.0.4 and later,
+    limited support in earlier versions.
+    No command data.
+    
+    Reply: ResponseData
+    
+        msgData ::= deviceMode | deviceMode deviceModeSub
+        deviceMode ::= uint16
+        deviceModeSub ::= uint16
+    
+    See src/common/device/DeviceMode.hpp for valid device mode values, and WSPRConfigFrame::startStatusUpdate for
+    hints on what they mean.
+    
+    deviceModeSub is only present for some deviceModes (currently, only DeviceMode::WSPR_Active).
+    
+    ### DeviceMode_Set
+    Sets the current device state.
+    
+    E.g. setting to DeviceMode::WSPR_Active has the same effect as pressing the button on the WSPRlite.
+    (This is currently unimplemented in the config program, since the config program does not yet have a way of
+    checking the accuracy of the computer time.)
+    
+    Command data for most device modes is:
+    
+        msgData ::= deviceMode
+        deviceMode ::= uint16
+    
+    For DeviceMode::Test_ConstantTx, which temporarily makes the WSPRlite emit a constant tone for testing purposes:
+    
+        msgData ::= deviceMode frequency paBias
+        frequency ::= uint64
+        paBias ::= uint16
+    
+    `frequency` is the output frequency in Hz. `paBias` controls the gate bias for the power amplifier stage,
+    which affects the output power of the WSPRlite. It is a PWM duty cycle, range 0-1000.
+    
+    Reply: ACK or NACK
+    
+    ### Bootloader_State
+    Checks whether the device is in bootloader (firmware update) mode.
+    No command data.
+    
+    Reply: ResponseData.
+    
+        msgData ::= bootloaderMode
+        bootloaderMode ::= '\x00' | '\x01' | '\x02'
+    
+    0=in normal mode, 1=in bootloader mode, 2=in bootloader mode with no valid firmware present to reboot into.
+    
+    ### Bootloader_Enter
+    ### Bootloader_EraseAll,
+    ### Bootloader_ErasePage,
+    ### Bootloader_ProgramHexRec,
+    ### Bootloader_ProgramRow,
+    ### Bootloader_ProgramWord,
+    ### Bootloader_CRC,
+    ### Bootloader_ProgramResetAddr
+    
+    Currently undocumented since they are likely of limited interest. Note that you might break your WSPRlite
+    if you use these incorrectly, to the extent of needing to use a PICkit or similar to fix it.
+    
+    ### DumpEEPROM
+    Currently undocumented since it has not been properly tested yet, and might or might not remain in the firmware.
+    
+    ### WSPR_GetTime
+    Gets the total time since WSPR transmission was started (either by pressing the button or by sending a
+    DeviceMode_Set message). Supported by firmware v1.1.1 and later.
+    
+    Reply: ResponseData.
+    
+        msgData ::= milliseconds seconds minutes hours
+        milliseconds ::= uint16
+        seconds ::= uint8
+        minutes ::= uint8
+        hours ::= uint32
+    
+    ### TestCmd
+    An undocumented command which allows some fine grained direct control of the hardware
+    (e.g. set LED flash sequence, set RF output, get button status), used in factory testing.
 
 """
 
+import os,sys
 import serial
 import binascii
 import struct
+from enum import Enum
 from time import sleep
 
-g_device = '/dev/ttyUSB0'
-#g_device = 'COM5'
+#========================================================================
+# Enumerations transferred from the C++ Config program
+# The message types
+class MsgType(Enum):
+    Version = 0
+    NACK = 1
+    ACK = 2
+    Read = 3
+    ResponseData = 4
+    Write = 5
+    Reset = 6
+    Bootloader_State = 7
+    Bootloader_Enter = 8
+    Bootloader_EraseAll = 9
+    Bootloader_ErasePage = 10
+    Bootloader_ProgramHexRec = 11
+    Bootloader_ProgramRow = 12
+    Bootloader_ProgramWord = 13
+    Bootloader_CRC = 14
+    Bootloader_ProgramResetAddr = 15
+    DeviceMode_Get = 16
+    DeviceMode_Set = 17
+    DumpEEPROM = 18
+    WSPR_GetTime = 19
+    TestCmd = 20
 
+# The message qualifier
+class VarId(Enum):
+    MemVersion = 0
+    xoFreq = 1
+    xoFreqFactory = 2
+    ChangeCounter = 3
+    DeviceId = 4
+    DeviceSecret = 5
+    WSPR_txFreq = 6
+    WSPR_locator = 7
+    WSPR_callsign = 8
+    WSPR_paBias = 9
+    WSPR_outputPower = 10
+    WSPR_reportPower = 11
+    WSPR_txPct = 12
+    WSPR_maxTxDuration = 13
+    CwId_Freq = 14
+    CwId_Callsign = 15
+    PaBiasSource = 16
+    END = 18
+
+# The device mode
+class VarId(Enum):
+    Init = 0
+    WSPR_Pending = 1
+    WSPR_Active = 2
+    WSPR_Invalid = 3
+    Test_ConstantTx = 4
+    FactoryInvalid = 5
+    HardwareFail = 6
+    FirmwareError = 7
+    WSPR_MorseIdent = 8
+    Test = 9
+
+#========================================================================
+"""
+    Main device class for WSPRLite
+"""
 class WSPRLite(object):
     
-    def __init__(self):
-        
+    #----------------------------------------------
+    # Constructor
+    def __init__(self, device):
         # Create connection and set parameters according to device spec
-        self.__ser = serial.Serial(g_device)
+        self.__ser = serial.Serial(device)
         self.__ser.baudrate = 1000000
         self.__ser.bytesize = 8
         self.__ser.parity = 'N'
@@ -138,18 +310,10 @@ class WSPRLite(object):
         self.__ser.rtscts = True
         self.__ser.timeout = 2.0
     
-    """
-    ### Read
-
-        Read a config variable.
-        
-        Command data:
-        
-            msgData ::= variableId
-            variableId ::= uint16
-        
-        Reply: NACK or ResponseData. Contents of ResponseData will depend on which variable is being read - see cfgvars.md for details.
-    """
+    #----------------------------------------------
+    # Read methods
+    #----------------------------------------------
+    # Get current callsign
     def get_callsign(self):
         crc = self.calc_crc_32(b'\x03\x00\x08\x00')
         msg = bytearray(b'\x01\x03\x00\x08\x00')
@@ -157,13 +321,17 @@ class WSPRLite(object):
         self.__ser.write(msg)
         print(self.__ser.readline())
 
+    #----------------------------------------------
+    # Get current locator
     def get_locator(self):
         crc = self.calc_crc_32(b'\x03\x00\x07\x00')
         msg = bytearray(b'\x01\x03\x00\x07\x00')
         msg = msg + crc + bytearray(b'\x04')
         self.__ser.write(msg)
         print(self.__ser.readline())
-        
+    
+    #----------------------------------------------
+    # Get current transmit frequency
     def get_freq(self):
         crc = self.calc_crc_32(b'\x03\x00\x06\x00')
         msg = bytearray(b'\x01\x03\x00\x06\x00')
@@ -173,24 +341,12 @@ class WSPRLite(object):
         f = resp[4:12]
         print(struct.unpack('<Q',f)[0])
     
-    """
-    ### Write
-
-        Write a config variable.
-        
-        Command data:
-        
-            msgData ::= variableId variableData
-            variableId ::= uint16
-        
-        variableData will depend on which variable is being written - see cfgvars.md for details.
-        
-        Reply: ACK or NACK.
-    """
-    
+    #----------------------------------------------
+    # Write methods
+    #----------------------------------------------
+    # Set the transmit frequency
+    # Freq is a float. This needs to be a 64 bit byte array in LE
     def set_freq(self, freq):
-        # Freq is a float
-        # This needs to be a 64 bit byte array in LE
         f = int(freq*1000000)
         f_bytes = struct.pack('<Q', f)
         crc = self.calc_crc_32(b'\x05\x00\x06\x00' + f_bytes)
@@ -198,7 +354,10 @@ class WSPRLite(object):
         msg = msg + crc + bytearray(b'\x04')
         self.__ser.write(msg)
         print(self.__ser.readline())
-
+    
+    #----------------------------------------------
+    # Start transmitting
+    # Note this must be correctly timed to an accurate clock
     def set_tx(self):
         crc = self.calc_crc_32(b'\x11\x00\x02\x00')
         msg = bytearray(b'\x01\x11\x00\x02\x00')
@@ -206,22 +365,39 @@ class WSPRLite(object):
         self.__ser.write(msg)
         print(self.__ser.readline())
     
-    def reset(self):
+    #----------------------------------------------
+    # Stop transmitting
+    # Note this should be done immediately after a transmission, not during tramsmission
+    def set_idle(self):
         crc = self.calc_crc_32(b'\x06\x00')
         msg = bytearray(b'\x01\x06\x00')
         msg = msg + crc + bytearray(b'\x04')
         self.__ser.write(msg)
         print(self.__ser.readline())
-        
+    
+    #----------------------------------------------
+    # Util methods
+    #----------------------------------------------
+    # Calculate a CRC32 of the given data
     def calc_crc_32(self, data):
         return struct.pack('<I', binascii.crc32(data))       
-        
-lite = WSPRLite()
-lite.get_callsign()
-lite.get_locator()
-lite.get_freq()
-lite.set_freq(7.097066)
-lite.get_freq()
-lite.set_tx()
-sleep(3)
-lite.reset()
+
+#========================================================================
+# Module Test       
+if __name__ == '__main__':
+    
+    if sys.platform == 'win32' or sys.platform == 'win64':
+        device = 'COM5'
+    else:
+        # Assume Linux
+        device = '/dev/ttyUSB0'
+    
+    lite = WSPRLite(device)
+    lite.get_callsign()
+    lite.get_locator()
+    lite.get_freq()
+    lite.set_freq(7.097066)
+    lite.get_freq()
+    lite.set_tx()
+    sleep(3)
+    lite.set_idle()
